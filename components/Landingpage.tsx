ce15b672b6fe79236bd0cf879325a602f025c31d
Anant Shanker <anant@MacBook-Air-3.local>
2023-06-09 17:46:49 +0800

    Updated Marketplace

--- components/Landingpage.tsx
+++ components/Landingpage.tsx
@@ -5,25 +5,7 @@ import { Container, Col, Row } from 'react-bootstrap';
 
 export default function LandingPage() {
     return (
-        // <section>
-        //     <Container>
-        //     <Row className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">
-        //         <Col>
-        //             <h1 className="mt-2 text-blue-800 text-2xl leading-8 font-extrabold sm:text-6xl text-left">
-        //                 Boundless Bazaar
-        //             </h1>
-        //         </Col>
-        //         <Col>
-        //             <h3 className="mt-2 text-green-700 text-2xl leading-8 font-extrabold sm:text-4xl text-left">
-        //                 Shop without limits.
-        //             </h3>
-        //         </Col>
-        //         <Col>
-        //             <Image src="/BB_icon.png" alt="" width={200} height={200} />
-        //         </Col>
-        //     </Row>
-        //     </Container>
-        // </section>
+        
         <div>
             <Row className="max-w-7xl mx-auto px-20 flex">
                  <Col className='pt-20 pr-80'>
